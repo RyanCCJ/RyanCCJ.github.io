508b4817770829a7387668041c0d249318cbba28
Ryan Chung <ryanccj@gmail.com>
2024-04-14 19:13:55 +0800

    update config

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, phone} = content
+  const { name, avatar, occupation, company, email, phone}=content
 
   return (
     <>
