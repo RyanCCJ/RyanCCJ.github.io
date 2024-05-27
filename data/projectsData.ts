16da32f12a891468f8c81e3de53018cd74d2837b
Ryan Chung <ryanccj@gmail.com>
2024-05-27 17:37:37 +0800

    add new posts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,7 +11,7 @@ const projectsData: Project[] = [
     description: `Biologists have always been curious about gene regulation mechanisms, 
     such as which proteins are involved in which reactions, the combination of RNAs in which locations, 
     and what outcomes are generated for target genes. This website may provide answers to your questions.`,
-    imgSrc: '/static/images/2024/sRNA_Analyst/tools.png',
+    imgSrc: '/static/images/2024/sRNA_Analyst/title.jpg',
     href: '/blog/2024/sRNA-Analyst',
   },
   {
@@ -27,7 +27,7 @@ const projectsData: Project[] = [
     description: `GDSC is a student community founded by Google to encourage students to apply Google technologies. 
     As core members of the NLP Group at NCKU, our main goal is to promote the use of natural language processing in 
     TensorFlow and PyTorch. We aim to create applications such as document summarization and ChatBot.`,
-    imgSrc: '/static/images/project/GDSC.jpg',
+    imgSrc: '/static/images/2023/GDSC/title.jpg',
     href: '/blog/2023/GDSC',
   },
   {
@@ -35,15 +35,15 @@ const projectsData: Project[] = [
     description: `In 2023, a group of young people went to Canada for an one-month alpine climbing training, 
     including snow, rock, and ice climbing skills. Thanks to the sponsorship and selection by Taiwan Climbing 
     & Mountaineering Alliance, our horizons are gradually expanding.`,
-    imgSrc: '/static/images/project/Canada.jpg',
+    imgSrc: '/static/images/2023/seed-project/title.jpg',
     href: '/blog/2023/seed-project',
   },
   {
-    title: 'Industry-Academia Collaboration: Internal Website Infrastructure',
+    title: 'Industry-Academia Collaboration: Website Infrastructure',
     description: `This project will assist a company in Taiwan with its internal transformation, 
     moving its AWS website to servers. It involves planning several Linux servers for load balancing and 
     database read-write separation, as well as redesigning the website's front-end and back-end architecture.`,
-    imgSrc: '/static/images/project/web_infra.jpg',
+    imgSrc: '/static/images/2022/web-infra/title.jpg',
     href: '/blog/2022/web-infra',
   },
   {
@@ -51,7 +51,7 @@ const projectsData: Project[] = [
     description: `Even today, many companies and government agencies continue to use macros in Word and Excel. 
     However, macros can pose significant security risks. This project will explore and implement potential 
     hacking techniques and defense mechanisms.`,
-    imgSrc: '/static/images/project/hacker.jpg',
+    imgSrc: '/static/images/2022/VBA-macros-attack/title.jpg',
     href: '/blog/2022/VBA-macros-attack',
   },
   {
@@ -59,7 +59,7 @@ const projectsData: Project[] = [
     description: `Due to the critical issue of COVID-19, we aim to utilize image recognition and AI 
     technology to assess the likelihood of illness from CT scans. Additionally, we are conducting some 
     interesting analyses related to whether people are correctly wearing masks based on facial recognition.`,
-    imgSrc: '/static/images/project/COVID-19.jpg',
+    imgSrc: '/static/images/2022/image-recognition/title.jpg',
     href: '/blog/2022/image-recognition',
   },
   {
@@ -68,23 +68,24 @@ const projectsData: Project[] = [
     Department to develop a smart dumbbell that can detect whether the movements of rehabilitation patients 
     are correct. This project was part of the 'Dreams Come True' initiative and we successfully presented 
     our paper in Indonesia.`,
-    imgSrc: '/static/images/project/dumbbell.png',
-    href: '/blog/2021/smart-dumbbell',
+    imgSrc: '/static/images/2019/smart-dumbbell/title.png',
+    href: '/blog/2019/smart-dumbbell',
   },
   {
-    title: 'JAM: Integrated Club Management Platform',
-    description: `Based on the needs of our club management, we wanted a platform that could integrate a 
-    shared calendar, cloud storage, photo albums, and a voting system in one place. That's how JAM was born.`,
-    imgSrc: '/static/images/project/JAM.png',
-    href: '/blog/2021/JAM',
+    title: 'Blogging Platform',
+    description: `This project demonstrates how to use the Django full-stack framework for creating a blog. 
+    It includes frontend template and form design, backend SQL database setup, user login functionality, 
+    and website deployment.`,
+    imgSrc: '/static/images/2021/Django/title.jpg',
+    href: '/blog/2021/DjangoI',
   },
   {
     title: 'Guide and Tour Commentary System',
     description: `Find Deqingxi is a mobile web app that integrates local culture and provides guided 
     services for the surroundings of Deqingxi. It allows users to navigate through the past and present maps, 
     interact with physical guides, and leave comments.`,
-    imgSrc: '/static/images/project/Deqingxi.png',
-    href: '/blog/2021/Find-Deqingxi',
+    imgSrc: '/static/images/2018/Find-Deqingxi/title.png',
+    href: '/blog/2018/Find-Deqingxi',
   },
   {
     title: 'Resonant Circuits for Wireless Power Transfer',
